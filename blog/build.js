4fa4691a759e26134f41881109771966438d6b54
JerryChaos <jerry-chaos@outlook.com>
2026-07-12 19:43:48 +1200

    Add 'One Tree Hill' post and update blog build
    Add new blog post posts/one_tree_hill.html with four images (blog/assets/one_tree_hill_*.jpg). Update blog/index.html to include and reorder the new post. Improve blog/build.js: refactor make_li to accept file proxies, add gatherValidFiles to collect/sort files by creation time, and use proxies when generating list items. Small tweaks: set img width=100% in posts/template.html and add XnView MP to softwares.html.

--- blog/build.js
+++ blog/build.js
@@ -11,30 +11,51 @@ console.log("Building Blog Content...");
 console.log(`   Current build file location: ${__dirname}`);
 const dir = path.join(__dirname, "posts");
 
-function make_li(tpl, dir, name) {
-    const _paper = fs.readFileSync(path.join(dir, name), "utf8");
+function make_li(tpl, proxy) {
+    const _paper = fs.readFileSync(proxy.url, "utf8");
     const _d = regexp_date.exec(_paper)[2];
     const _t = regexp_title.exec(_paper)[2];
     const _desc = regexp_description.exec(_paper)[2];
     tpl = tpl.replaceAll("{{date}}", _d);
-    tpl = tpl.replaceAll("{{url}}", name);
+    tpl = tpl.replaceAll("{{url}}", proxy.name);
     tpl = tpl.replaceAll("{{title}}", _t);
     tpl = tpl.replaceAll("{{description}}", _desc);
     return tpl;
 }
 
+
+function gatherValidFiles(parentFolder) {
+    const files = [];
+    const entries = fs.readdirSync(parentFolder, { withFileTypes: true });
+    for (const entry of entries) {
+        if (!entry.isFile()) continue;
+
+        const fileURL = path.join(parentFolder, entry.name);
+        const stats = fs.statSync(fileURL);
+
+        files.push({
+            name: entry.name,
+            url: fileURL,
+            created: stats.birthtime,
+            createdMs: stats.birthtimeMs,
+            modified: stats.mtime,
+            stats,
+        });
+    }
+
+    // Sort by creation date (oldest first)
+    files.sort((a, b) => b.createdMs - a.createdMs);
+
+    return files;
+}
+
 const html = fs.readFileSync(path.join(__dirname, "_index_"), "utf8");
 const tpl = html.match(regex_tpl);
 // console.log(tpl);
-const entries = fs.readdirSync(dir, { withFileTypes: true });
-
+const validFiles = gatherValidFiles(dir)
 let _content = '';
-for (const entry of entries) {
-    if (entry.isFile()) {
-        // console.log(dir + entry.name);
-        _content += make_li(String(tpl), dir, entry.name)
-    }
-    // console.log(_content)
+for (const proxy of validFiles) {
+    _content += make_li(String(tpl), proxy)
 }
 
 const result = html.replace(tpl, _content);
